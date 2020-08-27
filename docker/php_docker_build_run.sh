0be6b7a364aa50b0b70e3afffe3be2a7d206c39d
BohriumKwong <bohrium.kwong@gmail.com>
2020-08-27 13:51:43 +0800

    增加修改nginx配置文件的语句

--- docker/php_docker_build_run.sh
+++ docker/php_docker_build_run.sh
@@ -11,3 +11,5 @@ vi nginx/conf/conf.d/ai_rom_test_php.conf
 cat nginx/conf/conf.d/ai_rom_test_php.conf
 
 docker run --name ai_rom_php_test -p 8083:80 -d -v /media/totem_disk/totem/kwong/ai_rom_demo/www:/usr/share/nginx/html:ro -v /media/totem_disk/totem/kwong/ai_rom_demo/nginx/conf/conf.d:/etc/nginx/conf.d:ro --link myphp-fpm:php nginx
+
+docker cp ../nginx/nginx.conf nginx:/etc/nginx/nginx.conf

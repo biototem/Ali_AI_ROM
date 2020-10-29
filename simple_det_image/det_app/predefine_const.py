ee3176b2044da9665daa241c1497406244d0e31f
BohriumKwong <bohrium.kwong@gmail.com>
2020-10-29 18:27:03 +0800

    增加预定义任务类型信息

--- simple_det_image/det_app/predefine_const.py
+++ simple_det_image/det_app/predefine_const.py
@@ -2,22 +2,32 @@
 预定义消息字符串
 '''
 
-TYPE_DET_TASK_A = ['2','3','4','5','6','7']
-TYPE_DET_TASK_B = ['1']
+TYPE_DET_TASK_A = [str(x) for x in range(11,27)]
+TYPE_DET_TASK_B = [str(x) for x in range(1,9)]
 #SHOULDER_TASK = '1'
 ##正面右肩关节外展测量
-#Thumb_RADIALIS_Abduction_TASK = '2'
+#Thumb_RADIALIS_Abduction_TASK = '11'
 ##拇指桡侧关节外展测量
-#Thumb_CMC_Extension_TASK = '3'
+#Thumb_CMC_Extension_TASK = '12'
 ##拇指掌腕关节伸直测量
-#Thumb_CMC_Flexion_TASK = '4'
+#Thumb_CMC_Flexion_TASK = '13'
 ##拇指掌腕关节屈曲测量
-#Thumb_CMC_Abduction_TASK = '5'
+#Thumb_CMC_Abduction_TASK = '14'
 ##拇指掌腕关节外展测量
-#Thumb_MCP_Flexion_and_Extension_TASK = '6'
+#Thumb_MCP_Flexion_and_Extension_TASK = '15'
 ##拇指掌指关节屈曲伸直测量
-#Thumb_IP_Flexion_and_Extension_TASK = '7'
+#Thumb_IP_Flexion_and_Extension_TASK = '16'
 ##拇指指间关节屈曲伸直测量
+#Finger_PIP_Flexion_and_Extension_TASK = '21'
+##食指近端指关节屈曲伸直测量
+#Finger_DIP_Flexion_and_Extension_TASK = '22'
+##食指远端指关节屈曲伸直测量
+#Finger_MCP_Adduction_TASK = '23'
+##食指掌指关节内收测量任务
+#Finger_MCP_Adduction_TASK_2 = '24'
+##食指掌指关节外展测量任务
+#Finger_MP_Flexion_TASK = '25'
+##食指掌指关节屈曲测量任务
 TYPE_FILE_IMAGE = 'image'
 TYPE_FILE_VIDEO = 'video'
 
